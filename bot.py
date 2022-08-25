9fde210d0725a4552f7ffa21d1b42a2f7cc577df
AaKaSh <84575647+aakash2021-cap@users.noreply.github.com>
2022-08-26 00:43:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,8 @@ API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 API_KEY = environ.get('API_KEY')
 API_URL = environ.get('API_URL')
-
+API_URL1 = environ.get('API_URL1')
+API_KEY1 = environ.get('API_KEY1')
 akbotz = Client('link shortener bot',
              api_id=API_ID,
              api_hash=API_HASH,
@@ -44,6 +45,37 @@ async def link_handler(bot, message):
             await message.reply(f'𝐄𝐫𝐫𝐨𝐫: `{e}`', quote=True)
 
 
+            
+            
+            
+            
+@akbotz.on_message(filters.private & filters.text & filters.incoming)
+
+async def link_handler(bot, message):
+
+    link_pattern = re.compile('https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}', re.DOTALL)
+
+    links = re.findall(link_pattern, message.text)
+
+    if len(links) <1:
+
+        await message.reply("No links Found in this text",quote=True)
+
+        return
+
+    for link in links:
+
+        try:
+
+            short_link = await get_shortlink(link)
+
+            await message.reply(f"𝐇𝐞𝐫𝐞 𝐢𝐬 𝐘𝐨𝐮𝐫 𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤\n\n𝐎𝐫𝐢𝐠𝐢𝐧𝐚𝐥 𝐋𝐢𝐧𝐤: {link}\n\n𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤: `{short_link}`",quote=True,disable_web_page_preview=True)
+
+        except Exception as e:
+
+            await message.reply(f'𝐄𝐫𝐫𝐨𝐫: `{e}`', quote=True)
+            
+            
 async def get_shortlink(link):
     url = API_URL
     params = {'api': API_KEY, 'url': link}
